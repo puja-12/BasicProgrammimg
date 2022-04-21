b7a09cf6e315ffbde1760929c6c134155f63371b
puja <pujarana1796@gmail.com>
2022-04-21 12:53:25 +0530

    leapyear

--- Basicprogramming/Program.cs
+++ Basicprogramming/Program.cs
@@ -1,28 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-public class Vowels
+namespace Basicprogramming
 {
-    static void Main(string[] args)
+    class Program
+
     {
-        char ch;
-
-        Console.WriteLine("Enter any character: ");
-        ch = Convert.ToChar(Console.ReadLine());
-
-
-        // Condition for vowel checking
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+        static void Main(string[] args)
         {
-
-            Console.WriteLine(ch + " is Vowel.");
-
+            Uc1 leap = new Uc1();
+            leap.checkLeapyear();
         }
-        else
-        {
-            Console.WriteLine(ch + " is Consonant.");
-        }
-
-        Console.ReadLine();
     }
 }
---
+++ Basicprogramming/Uc1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basicprogramming
+{
+   class Uc1
+    {
+        public void checkLeapyear()
+        {
+            Console.WriteLine("enter year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                Console.WriteLine("leap year");
+                    }
+            else
+            {
+                Console.WriteLine("not leap year");
+            }
+             
+        }
+    }
+}
