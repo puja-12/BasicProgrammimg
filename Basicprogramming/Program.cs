af50f38ee1794ac324aaa0edcbad46289916b5e1
puja <pujarana1796@gmail.com>
2022-04-21 16:23:41 +0530

    evenodd

--- Basicprogramming/Program.cs
+++ Basicprogramming/Program.cs
@@ -8,8 +8,24 @@ namespace Basicprogramming
     {
         static void Main(string[] args)
         {
-            Uc1 leap = new Uc1();
-            leap.checkLeapyear();
+
+            while (true)
+            {
+                Console.WriteLine("select number\n1)Leap year\n2)even odd\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Uc1 leap = new Uc1();
+                        leap.checkLeapyear();
+                        break;
+                    case 2:
+                        Uc2 evenodd = new Uc2();
+                        evenodd.evenOdd();
+                        break;
+                }
+
+            }
         }
     }
 }
---
+++ Basicprogramming/Uc2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basicprogramming
+{
+    public class Uc2
+    {
+        public void evenOdd()
+        {
+                Console.WriteLine("enter number");
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine("even number ");
+                }
+                else
+                {
+                    Console.WriteLine("odd number");
+                }
+
+            }
+    }
+}
